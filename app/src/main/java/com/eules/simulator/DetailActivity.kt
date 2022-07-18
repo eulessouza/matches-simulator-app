b9ed8f74c05917e6c6a006dc20b7986d09137877
queixo <necrogamer01@gmail.com>
2022-07-18 13:10:10 -0300

    Desenvolvendo os layouts de listagem e detalhes das partidas.

---
+++ app/src/main/java/com/eules/simulator/DetailActivity.kt
@@ -0,0 +1,20 @@
+package com.eules.simulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.eules.simulator.databinding.ActivityDetailBinding
+import com.eules.simulator.databinding.ActivityMainBinding
+
+class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
